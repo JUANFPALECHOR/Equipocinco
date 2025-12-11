ceed743d6542cf9b96a4d4a0425a9f64149d1afd
Camilou198 <esteban.urbano@correounivalle.edu.co>
2025-12-10 21:55:09 -0500

    saldo actualizado en el widget

--- app/src/main/java/com/univalle/inventorywidget/di/DatabaseModule.kt
+++ app/src/main/java/com/univalle/inventorywidget/di/DatabaseModule.kt
@@ -1,9 +1,11 @@
 package com.univalle.inventorywidget.di
 
+import android.content.Context
 import com.google.firebase.firestore.FirebaseFirestore
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
+import dagger.hilt.android.qualifiers.ApplicationContext
 import dagger.hilt.components.SingletonComponent
 import javax.inject.Singleton
 
@@ -15,5 +17,11 @@ object DatabaseModule {
     @Singleton
     fun provideFirebaseFirestore(): FirebaseFirestore {
         return FirebaseFirestore.getInstance()
+    }
+
+    @Provides
+    @Singleton
+    fun provideApplicationContext(@ApplicationContext context: Context): Context {
+        return context
     }
 }
